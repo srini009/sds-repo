d36d3d362a962302fb45aad749948f4d4669de04
Srinivasan Ramesh <sramesh@cs.uoregon.edu>
2021-01-10 14:02:51 +0000

    Add symbiomon spack package

---
+++ packages/mochi-symbiomon/package.py
@@ -0,0 +1,23 @@
+from spack import *
+
+
+class MochiSymbiomon(CMakePackage):
+    """SYMBIOMON is a Mochi microservice designed to capture and export metrics in 
+    time-series data format over RDMA. It is primarily intended for remote-monitoring
+    purposes."""
+
+    homepage = "https://github.com/srini009/symbiomon"
+    url = "https://github.com/srini009/symbiomon"
+    git='https://github.com/srini009/symbiomon.git'
+
+    version('develop', branch='master')
+
+    depends_on('libuuid')
+    depends_on('mochi-margo@develop')
+    depends_on('mercury@develop')
+    depends_on('argobots@main')
+    depends_on('mochi-abt-io@0.5:')
+
+    def cmake_args(self):
+        args = ["-DBUILD_SHARED_LIBS:BOOL=ON" ]
+        return args
