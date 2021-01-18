36c428fe730ead2ccce7a69dd1fc9093ab2d0ab0
Srinivasan Ramesh <ramesh.srinivasan09@gmail.com>
2021-01-18 04:52:57 -0500

    Build examples by default

--- packages/mochi-symbiomon/package.py
+++ packages/mochi-symbiomon/package.py
@@ -15,9 +15,10 @@ class MochiSymbiomon(CMakePackage):
     depends_on('libuuid')
     depends_on('mochi-margo@develop')
     depends_on('mercury@master')
-    depends_on('argobots@main')
+    #depends_on('argobots@main')
+    depends_on('argobots@1.0:')
     depends_on('mochi-abt-io@0.5:')
 
     def cmake_args(self):
-        args = ["-DBUILD_SHARED_LIBS:BOOL=ON" ]
+        args = ["-DBUILD_SHARED_LIBS:BOOL=ON -DENABLE_EXAMPLES:BOOL=ON" ]
         return args
