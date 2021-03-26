c2098f24bf50d12fd0bd66174818c333a7a27647
Srinivasan Ramesh <ramesh.srinivasan09@gmail.com>
2021-03-26 15:29:59 -0400

    Add aggregator spack changes

--- packages/mochi-symbiomon/package.py
+++ packages/mochi-symbiomon/package.py
@@ -12,13 +12,18 @@ class MochiSymbiomon(CMakePackage):
 
     version('develop', branch='master')
 
+    variant('aggregator', default=True, description='Aggregate time-series data')
+
     depends_on('libuuid')
     depends_on('mochi-margo@develop')
     depends_on('mercury@master')
     #depends_on('argobots@main')
     depends_on('argobots@1.0:')
+    depends_on('mochi-develop@develop', when='+aggregator')
     depends_on('mochi-abt-io@develop')
 
     def cmake_args(self):
         args = ["-DBUILD_SHARED_LIBS:BOOL=ON -DENABLE_EXAMPLES=ON" ]
+        if '+aggregator' in self.spec:
+            args.append('-DENABLE_AGGREGATOR=ON')
         return args
